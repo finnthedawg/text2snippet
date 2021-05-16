5009dc73f10a2fe6a5a2d4e8a68fb79531a5ff30
finnthedawg <42992404+finnthedawg@users.noreply.github.com>
2021-05-16 20:26:33 +0400

    Changes

--- tests/code_test.py
+++ tests/code_test.py
@@ -55,55 +55,58 @@ testcases = [{
     },
     {
         'question' : "count the occurrences of items in {var_name}}",
-        'data' : [strings, lists, strings],
+        'data' : strings + lists + strings,
         'code' : "count({var_name})"
     },
     {
         'question' : " Get duplicate elements from list {var_name}",
-        'data' : [lists],
+        'data' : lists,
         'code' : "[{var_name}[i] for i in range(len({var_name})) if not i == {var_name}.index({var_name}[i])]"
     },
     {
         'question' : 'Generate a list containing consecutive numbers between 5 and 10',
-        'data' : [lists],
+        'data' : lists,
         'code' :  "[x for x in range(5, 11)]"
     },
     {
         'question' : "Get the element of list {var_name} at index 1",
-        'data' : [lists],
+        'data' : lists,
         'code' : "a_val[2]"
-    }
-    #iterate through dictionary "a_val"
+    },
     
     #Add filler word to end.
     #Change variable names
 ]
 
 
+def runtest(testcase):
+    question = testcase['question']
+    data = testcase['data']
+    code = testcase['code']
+    for d in data:
+        eval("{var_name} = {d}")
+        res = eval(code)
+        print(res)
+    return
 
-def t1():
-    ans = []
-    for data in testcases[0]['data']:
-        x = data
-        res = eval(testcases[0]['code'])
-        ans.append(res)
-    return ans
+for ts in testcases:
+    runtest(ts)
+    break
+
+# def t1():
+#     ans = []
+#     for data in testcases[0]['data']:
+#         x = data
+#         res = eval(testcases[0]['code'])
+#         ans.append(res)
+#     return ans
 
 
-class TestAddFishToAquarium(unittest.TestCase):
-    def testcase1(self):
-        truth = t1()
-        ans = []
-        for data in testcases[0]['data']:
-            ans.append(testcases[0]['question'].replace('a_val', data))
+# class RunComparison(unittest.TestCase):
+#     def testcase1(self):
+#         truth = t1()
+#         ans = []
+#         for data in testcases[0]['data']:
+#             ans.append(testcases[0]['question'].replace('a_val', data))
 
-        print(ans, truth)
-
-    # def test_add_fish_to_aquarium_exception(self):
-    #     too_many_fish = ["shark"] * 25
-    #     with self.assertRaises(ValueError) as exception_context:
-    #         add_fish_to_aquarium(fish_list=too_many_fish)
-    #     self.assertEqual(
-    #         str(exception_context.exception),
-    #         "A maximum of 10 fish can be added to the aquarium"
-    #     )
+#         print(ans, truth)
